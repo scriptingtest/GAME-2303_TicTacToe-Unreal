7d417dc9446ecf8523a44948fff7aaea3bf580bc
scriptingtest <96097017+scriptingtest@users.noreply.github.com>
2022-11-09 12:49:49 -0600

    Move project files to root
    - Moved project files of converted project to root of repository
    - Removed original project folder and files

--- TTT 4.27/Intermediate/Build/Win64/TTTEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30146
-#include "F:/repository/GAME-2303_TicTacToe/TTT 4.27/Intermediate/Build/Win64/TTTEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- TTT/Intermediate/Build/Win64/TTTEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:/Projects/Unreal Projects/TTT/Source/TicTacToe/TTT/Intermediate/Build/Win64/TTTEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
